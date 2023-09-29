c478dcf224e809e45ba1f6db74913cefbc647860
adam <adamsoimansoibpro@gmail.com>
2023-09-29 17:11:55 +0200

    Add Teioc back module.

---
+++ Teioc-back/src/test/java/dev/astranfalio/teioc/TeiocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.astranfalio.teioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeiocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
