6ba0bf1e8e4b0126fb1aaff3df69acc5ef939f43
Yaathavan <yaathavan.kumarasamy@etu.utc.fr>
2023-11-24 21:37:46 +0100

    Answer and Question Service

--- src/main/java/dev/astranfalio/teioc/controller/AnswerController.java
+++ src/main/java/dev/astranfalio/teioc/controller/AnswerController.java
@@ -1,19 +1,63 @@
 package dev.astranfalio.teioc.controller;
 
+import dev.astranfalio.teioc.dto.AnswerDto;
 import dev.astranfalio.teioc.entity.AnswerEntity;
-import dev.astranfalio.teioc.repository.AnswerRepository;
+import dev.astranfalio.teioc.repository.QuestionRepository;
+import dev.astranfalio.teioc.service.AnswerDataService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/answers")
 public class AnswerController {
+    private final AnswerDataService answerDataService;
+    private final QuestionRepository questionRepository;
 
     @Autowired
-    private AnswerRepository answerRepository;
+    public AnswerController(AnswerDataService answerDataService, QuestionRepository questionRepository) {
+        this.answerDataService = answerDataService;
+        this.questionRepository = questionRepository;
+    }
 
-    @GetMapping("/answers")
-    public List<AnswerEntity> getAnswers() {
-        return answerRepository.findAll();
+    @GetMapping
+    public List<AnswerDto> getAllAnswers() {
+        return answerDataService.findAll().stream()
+                .map(AnswerDto::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnswerDto> getAnswerById(@PathVariable Long id)
+    {
+        AnswerEntity answerEntity = answerDataService.findById(id);
+        AnswerDto answerDto = AnswerDto.convertToDto(answerEntity);
+        return ResponseEntity.ok(answerDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<AnswerDto> addAnswer(@Valid @RequestBody AnswerDto answerDto) {
+        AnswerEntity answerEntity = AnswerDataService.convertToEntity(answerDto, questionRepository);
+        AnswerEntity savedEntity = answerDataService.save(answerEntity);
+        AnswerDto savedDto = AnswerDto.convertToDto(savedEntity);
+        return ResponseEntity.ok(savedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAnswer(@PathVariable Long id) {
+        answerDataService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AnswerDto> updateAnswer(@PathVariable Long id, @Valid @RequestBody AnswerDto answerDto) {
+        AnswerEntity answerEntity = AnswerDataService.convertToEntity(answerDto, questionRepository);
+        AnswerEntity updatedEntity = answerDataService.update(id, answerEntity);
+        AnswerDto updatedDto = AnswerDto.convertToDto(updatedEntity);
+        return ResponseEntity.ok(updatedDto);
     }
 }
--- src/main/java/dev/astranfalio/teioc/controller/QuestionController.java
+++ src/main/java/dev/astranfalio/teioc/controller/QuestionController.java
@@ -1,20 +1,66 @@
 package dev.astranfalio.teioc.controller;
 
+import dev.astranfalio.teioc.dto.QuestionDto;
 import dev.astranfalio.teioc.entity.QuestionEntity;
-import dev.astranfalio.teioc.repository.QuestionRepository;
+import dev.astranfalio.teioc.repository.AnswerRepository;
+import dev.astranfalio.teioc.repository.SurveyRepository;
+import dev.astranfalio.teioc.service.QuestionDataService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/questions")
 public class QuestionController {
+    private final QuestionDataService questionDataService;
+    private final SurveyRepository surveyRepository;
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    private QuestionRepository questionRepository;
+    public QuestionController(QuestionDataService questionDataService, SurveyRepository surveyRepository, AnswerRepository answerRepository) {
+        this.questionDataService = questionDataService;
+        this.surveyRepository = surveyRepository;
+        this.answerRepository = answerRepository;
+    }
 
-    @GetMapping("/questions")
-    public List<QuestionEntity> getQuestions() {
-        return questionRepository.findAll();
+    @GetMapping
+    public List<QuestionDto> getAllQuestions() {
+        return questionDataService.findAll().stream()
+                .map(QuestionDto::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionDto> getQuestionById(@PathVariable Long id)
+    {
+        QuestionEntity questionEntity = questionDataService.findById(id);
+        QuestionDto questionDto = QuestionDto.convertToDto(questionEntity);
+        return ResponseEntity.ok(questionDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<QuestionDto> addQuestion(@Valid @RequestBody QuestionDto questionDto) {
+        QuestionEntity questionEntity = QuestionDataService.convertToEntity(questionDto, surveyRepository, answerRepository);
+        QuestionEntity savedEntity = questionDataService.save(questionEntity);
+        QuestionDto savedDto = QuestionDto.convertToDto(savedEntity);
+        return ResponseEntity.ok(savedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteQuestion(@PathVariable Long id) {
+        questionDataService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<QuestionDto> updateQuestion(@PathVariable Long id, @Valid @RequestBody QuestionDto questionDto) {
+        QuestionEntity questionEntity = QuestionDataService.convertToEntity(questionDto, surveyRepository, answerRepository);
+        QuestionEntity updatedEntity = questionDataService.update(id, questionEntity);
+        QuestionDto updatedDto = QuestionDto.convertToDto(updatedEntity);
+        return ResponseEntity.ok(updatedDto);
     }
 }
