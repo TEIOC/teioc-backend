ed9ec0c7462178dfddf2afc1d9742d0bdc6706d4
abdel <dorra.abdelkefi@etu.utc.fr>
2023-11-25 15:49:08 +0100

    pathway answers

--- src/main/java/dev/astranfalio/teioc/controller/PathwayAnswerController.java
+++ src/main/java/dev/astranfalio/teioc/controller/PathwayAnswerController.java
@@ -1,19 +1,65 @@
 package dev.astranfalio.teioc.controller;
 
+import dev.astranfalio.teioc.dto.PathwayAnswerDto;
 import dev.astranfalio.teioc.entity.PathwayAnswerEntity;
-import dev.astranfalio.teioc.repository.PathwayAnswerRepository;
+import dev.astranfalio.teioc.entity.PathwayAnswerId;
+import dev.astranfalio.teioc.service.PathwayAnswerDataService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/pathwayanswers")
 public class PathwayAnswerController {
 
-    @Autowired
-    private PathwayAnswerRepository pathwayAnswerRepository;
+    private final PathwayAnswerDataService pathwayAnswerDataService;
 
-    @GetMapping("/pathwayanswers")
-    public List<PathwayAnswerEntity> getPathwayAnswers() {
-        return pathwayAnswerRepository.findAll();
+    @Autowired
+    public PathwayAnswerController(PathwayAnswerDataService pathwayAnswerDataService) {
+        this.pathwayAnswerDataService = pathwayAnswerDataService;
+    }
+
+    @GetMapping
+    public List<PathwayAnswerDto> getAllPathwayAnswers() {
+        return pathwayAnswerDataService.findAll().stream()
+                .map(PathwayAnswerDto::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{intern_id}/{survey_id}/{answer_id}")
+    public ResponseEntity<PathwayAnswerDto> getPathwayAnswerById(@PathVariable Integer intern_id, @PathVariable Integer survey_id, @PathVariable Integer answer_id) {
+        PathwayAnswerEntity pathwayAnswerEntity = pathwayAnswerDataService.findById(new PathwayAnswerId(intern_id, survey_id, answer_id));
+        PathwayAnswerDto pathwayAnswerDto = PathwayAnswerDto.convertToDto(pathwayAnswerEntity);
+        return ResponseEntity.ok(pathwayAnswerDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PathwayAnswerDto> addPathwayAnswer(@Valid @RequestBody PathwayAnswerDto pathwayAnswerDto) {
+        PathwayAnswerEntity pathwayAnswerEntity = pathwayAnswerDataService.convertToEntity(pathwayAnswerDto);
+        PathwayAnswerEntity savedEntity = pathwayAnswerDataService.save(pathwayAnswerEntity);
+        PathwayAnswerDto savedDto = PathwayAnswerDto.convertToDto(savedEntity);
+        return ResponseEntity.ok(savedDto);
+    }
+
+    @DeleteMapping("/{intern_id}/{survey_id}/{answer_id}")
+    public ResponseEntity<Void> deletePathwayAnswer(@PathVariable Integer intern_id, @PathVariable Integer survey_id, @PathVariable Integer answer_id) {
+        pathwayAnswerDataService.deleteById(new PathwayAnswerId(intern_id, survey_id, answer_id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{intern_id}/{survey_id}/{answer_id}")
+    public ResponseEntity<PathwayAnswerDto> updatePathwayAnswer(@PathVariable Integer intern_id, @PathVariable Integer survey_id, @PathVariable Integer answer_id, @Valid @RequestBody PathwayAnswerDto pathwayAnswerDto) {
+        pathwayAnswerDto.setIntern_id(intern_id);
+        pathwayAnswerDto.setSurvey_id(survey_id);
+        pathwayAnswerDto.setAnswer_id(answer_id);
+        PathwayAnswerEntity pathwayAnswerEntity = pathwayAnswerDataService.convertToEntity(pathwayAnswerDto);
+        PathwayAnswerEntity updatedEntity = pathwayAnswerDataService.update(new PathwayAnswerId(intern_id, survey_id, answer_id), pathwayAnswerEntity);
+        PathwayAnswerDto updatedDto = PathwayAnswerDto.convertToDto(updatedEntity);
+        return ResponseEntity.ok(updatedDto);
     }
 }
+
---
+++ src/main/java/dev/astranfalio/teioc/service/PathwayAnswerDataService.java
@@ -0,0 +1,52 @@
+package dev.astranfalio.teioc.service;
+
+import dev.astranfalio.teioc.dto.PathwayAnswerDto;
+import dev.astranfalio.teioc.entity.PathwayAnswerEntity;
+import dev.astranfalio.teioc.entity.PathwayAnswerId;
+import dev.astranfalio.teioc.repository.AnswerRepository;
+import dev.astranfalio.teioc.repository.InternRepository;
+import dev.astranfalio.teioc.repository.PathwayAnswerRepository;
+import dev.astranfalio.teioc.repository.SurveyRepository;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PathwayAnswerDataService extends AbstractDataService<PathwayAnswerEntity, PathwayAnswerId, PathwayAnswerRepository> {
+
+    private final InternRepository internRepository;
+    private final SurveyRepository surveyRepository;
+    private final AnswerRepository answerRepository;
+
+    @Autowired
+    public PathwayAnswerDataService(PathwayAnswerRepository pathwayAnswerRepository,
+                                    InternRepository internRepository,
+                                    SurveyRepository surveyRepository,
+                                    AnswerRepository answerRepository,
+                                    Validator validator) {
+        super(pathwayAnswerRepository, validator);
+        this.internRepository = internRepository;
+        this.surveyRepository = surveyRepository;
+        this.answerRepository = answerRepository;
+    }
+
+    @Transactional
+    public PathwayAnswerEntity convertToEntity(PathwayAnswerDto pathwayAnswerDto) {
+        PathwayAnswerId pathwayAnswerId = new PathwayAnswerId(pathwayAnswerDto.getIntern_id(),
+                pathwayAnswerDto.getSurvey_id(),
+                pathwayAnswerDto.getAnswer_id());
+
+        PathwayAnswerEntity pathwayAnswerEntity = new PathwayAnswerEntity();
+        pathwayAnswerEntity.setId(pathwayAnswerId);
+        pathwayAnswerEntity.setIntern(internRepository.findById(pathwayAnswerDto.getIntern_id())
+                .orElseThrow(() -> new ResourceNotFoundException("Intern not found with ID: " + pathwayAnswerDto.getIntern_id())));
+        pathwayAnswerEntity.setSurvey(surveyRepository.findById(pathwayAnswerDto.getSurvey_id())
+                .orElseThrow(() -> new ResourceNotFoundException("Survey not found with ID: " + pathwayAnswerDto.getSurvey_id())));
+        pathwayAnswerEntity.setAnswer(answerRepository.findById(pathwayAnswerDto.getAnswer_id())
+                .orElseThrow(() -> new ResourceNotFoundException("Answer not found with ID: " + pathwayAnswerDto.getAnswer_id())));
+
+        return pathwayAnswerEntity;
+    }
+}
+
