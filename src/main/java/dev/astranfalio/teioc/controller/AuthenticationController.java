1942f4b6145560f899e429b6cab83bcd6959c133
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2023-12-08 11:42:45 +0100

    Harmonize AuthenticationController. (Remove custom message in favor of default Controller Adivce message)

--- src/main/java/dev/astranfalio/teioc/controller/AuthenticationController.java
+++ src/main/java/dev/astranfalio/teioc/controller/AuthenticationController.java
@@ -4,14 +4,10 @@ import dev.astranfalio.teioc.config.JwtUtils;
 import dev.astranfalio.teioc.dto.LoginDto;
 import dev.astranfalio.teioc.service.InternDetailsService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,18 +21,16 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private JwtUtils jwtUtils;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticate(@RequestBody LoginDto loginDto) {
+    @PostMapping(value = "/login")
+    @ResponseBody
+    public Map<String, String> authenticate(@RequestBody LoginDto loginDto) {
         authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
+        );
         final UserDetails intern = internDetailsService.loadUserByUsername(loginDto.getEmail());
-        if (intern != null) {
-            String token = jwtUtils.generateToken(intern);
-            Map<String, String> tokenMap = new HashMap<>();
-            tokenMap.put("token", token);
-            return ResponseEntity.ok(tokenMap);
-        } else {
-            return ResponseEntity.badRequest().body("Utilisateur introuvable ou mot de passe incorrect");
-        }
+        String token = jwtUtils.generateToken(intern);
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        return tokenMap;
     }
 }
