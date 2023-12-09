90f5c5c471835ccdc489b155a4df0311fe90dbfe
Yaathavan <yaathavan.kumarasamy@etu.utc.fr>
2023-12-09 22:36:59 +0100

    Finished Register Workflow and feature

--- src/main/java/dev/astranfalio/teioc/config/SecurityConfig.java
+++ src/main/java/dev/astranfalio/teioc/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/interns/**").permitAll()
                         .requestMatchers("/email/reset-password").permitAll()
+                        .requestMatchers("/email/activate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
--- src/main/java/dev/astranfalio/teioc/controller/EmailTestController.java
+++ src/main/java/dev/astranfalio/teioc/controller/EmailTestController.java
@@ -17,29 +17,42 @@ import org.springframework.web.bind.annotation.*;
 public class EmailTestController {
 
     private EmailService emailService;
-
     private InternDataService internDataService;
+    private static final String SENDER_MAIL = "noreply@teioc.com";
+    private static final String SUCCES_RESPONSE = "Email sent successfully";
 
     @GetMapping("/send")
     @ResponseBody
     public String sendTestEmail() {
-        String from = "noreply@teioc.com";
+        String from = SENDER_MAIL;
         String to = "kumarasamy.yaathavan@hotmail.com";
         String subject = "Test Email";
         String body = "This is a test email from Spring Boot application.";
         emailService.sendSimpleMessage(from, to, subject, body);
-        return "Email sent successfully";
+        return SUCCES_RESPONSE;
     }
 
     @PostMapping("/reset-password")
-    public String resetPassword(@RequestBody InternEmailDto emailDto) {
-        String from = "noreply@teioc.com";
+    public String sendResetPasswordMail(@RequestBody InternEmailDto emailDto) {
+        String from = SENDER_MAIL;
         String email = emailDto.getEmail();
         InternDto intern = internDataService.findInternByEmail(email);
         String to = email;
         String subject = "[TEIOC] Reset Password for your account!";
         String body = "Here is the link to reset your account's password : http://localhost:3000/reset-password/" + intern.getId() + " \n If you didn't ask for the change, report to administrator.";
         emailService.sendSimpleMessage(from,to,subject,body);
-        return "Email sent successfully";
+        return SUCCES_RESPONSE;
+    }
+
+    @PostMapping("/activate")
+    public String sendActivationMail(@RequestBody InternEmailDto emailDto) {
+        String from = SENDER_MAIL;
+        String email = emailDto.getEmail();
+        InternDto intern = internDataService.findInternByEmail(email);
+        String to = email;
+        String subject = "[TEIOC] Activate your account!";
+        String body = "Here is the link to activate your account: http://localhost:3000/activate/" + intern.getId();
+        emailService.sendSimpleMessage(from,to,subject,body);
+        return SUCCES_RESPONSE;
     }
 }
