189c6978e4c5bc5594ae58522bd00da3551c41c8
abdel <dorra.abdelkefi@etu.utc.fr>
2023-12-10 23:03:30 +0100

    topics and surveys access for frontend

--- src/main/java/dev/astranfalio/teioc/config/SecurityConfig.java
+++ src/main/java/dev/astranfalio/teioc/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/interns").permitAll()
+                        .requestMatchers("/interns", "/topics", "/surveys").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
